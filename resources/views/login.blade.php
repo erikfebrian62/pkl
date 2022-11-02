a5c80030cc9e84e3cf22347e66e464cdc69d9555
erikfebrian62@smk.belajar.id <108683885+erikfebrian62@users.noreply.github.com>
2022-11-02 09:33:42 +0700

    ganti tampilan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Projek PKL</title>
-        <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="row justify-content-center" style="margin-top: 150px;">
-                <div class="col-md-4">
-                    <div class="card shadow-lg">
-                        <div class="card-body p-5">
-                            <h4 class="mt-3 text-center">E-Voting <br>OSIS</h4>
-                            @if(session('error'))
-                            <div class="alert alert-danger">
-                                {{session('error')}}
-                            </div>
-                            @endif
-                            <form action="" class="mt-4" method="post">
-                            @csrf
-                                <div class="mb-3">
-                                    <label for="nis" class="form-label">Masukan NIS</label>
-                                    <input type="number" class="form-control" name="nis" id="nis">
-                                </div>
-                                <div class="form-group">
-                                    <label for="password" class="form-label">Masukan Password</label>
-                                    <input type="password" name="password" id="password" class="form-control">
-                                </div>
 
-                                <button type="submit" class="btn btn-primary w-100 mt-4">Login</button>
-                            </form>
-                        </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Mazer Admin Dashboard</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+</head>
+
+<body>
+    <div id="auth">
+
+<div class="row h-100">
+    <div class="col-lg-5 col-12">
+        <div id="auth-left">
+            <h1 class="auth-title">E-voting OSIS.</h1>
+            <p class="auth-subtitle mb-5">Log in dengan data yang diberikan wali kelas.</p>
+
+            <form action="{{ route('login.proces') }}" method="POST">
+                @csrf
+                <div class="form-group position-relative has-icon-left mb-4">
+                    <input type="text" class="form-control form-control-xl" placeholder="NIS" name="nis">
+                    <div class="form-control-icon">
+                        <i class="bi bi-person"></i>
                     </div>
                 </div>
-            </div>
+                <div class="form-group position-relative has-icon-left mb-4">
+                    <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
+                    <div class="form-control-icon">
+                        <i class="bi bi-shield-lock"></i>
+                    </div>
+                </div>
+                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-3">Log in</button>
+            </form>
         </div>
-    </body>
+    </div>
+    <div class="col-lg-7 d-none d-lg-block">
+        <div id="auth-right">
+
+        </div>
+    </div>
+</div>
+
+    </div>
+</body>
+
 </html>
