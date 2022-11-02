58d226df31f2f0b80dc5d16279a6d1c54665fbf4
erikfebrian62@smk.belajar.id <108683885+erikfebrian62@users.noreply.github.com>
2022-11-02 11:37:58 +0700

    Membuat logic votes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Vote;
 
 class LoginController extends Controller
 {
@@ -16,6 +17,10 @@ class LoginController extends Controller
     {
         if(Auth::attempt(['nis' => $request->nis, 'password' => $request->password])) {
             if(Auth::user()->role === 'user') {
+                $vote = Vote::where('user_id', Auth::user()->id)->count();
+                if($vote > 0) {
+                    return redirect(route('user.informasi-suara'));
+                }
                 return redirect(route('user.pilih-kandidat'));
             }else{
                 return redirect(route('admin.dashboard'));
