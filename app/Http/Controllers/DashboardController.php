4dee197020eeecf477970b1d525c7afce7b3cd97
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2022-11-15 12:36:25 +0700

    menambahkan vote->count pada dashboard admin dan menambahkan model di controllerdashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Vote;
 
 class DashboardController extends Controller
 {
     public function dash()
     {
+        $vote = Vote::get();
         $users = User::where('role', 'user')->get();
-        return view('admin.dashboard.index',[ 'title' => 'Dashboard-Admin'], compact('users'));
+        return view('admin.dashboard.index',[ 'title' => 'Dashboard-Admin'], compact('users', 'vote'));
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -34,7 +34,7 @@ Dasboard
                     </div>
                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                         <h6 class="text-muted font-semibold">Sudah Voting</h6>
-                        <h6 class="font-extrabold mb-0">183.000</h6>
+                        <h6 class="font-extrabold mb-0 ">{{ $vote->count() }}</h6>
                     </div>
                 </div>
             </div>
