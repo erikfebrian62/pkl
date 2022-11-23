b216ab23b8f3d61cb31a0fbbd693e1e2b3ce8ae3
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2022-11-23 21:50:05 +0700

    menambahkan gambar pada page lgin

--- resources/views/layouts/mainadmin.blade.php
+++ resources/views/layouts/mainadmin.blade.php
@@ -8,8 +8,8 @@
 
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main/app-dark.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('assets/css/shared/iconly.css') }}">
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,50 +7,55 @@
     <title>Login E-Voting OSIS</title>
     <link rel="stylesheet" href="{{ asset('assets/css/main/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/pages/auth.css') }}">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.svg') }}" type="image/x-icon">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo/favicon.png') }}" type="image/png">
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/logo.png') }}" type="image/png">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
     <div id="auth">
 
-<div class="row h-100">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <h1 class="auth-title">E-voting OSIS.</h1>
-            <p class="auth-subtitle mb-5">Login dengan data yang diberikan wali kelas.</p>
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <h1 class="auth-title">E-voting OSIS.</h1>
+                    <p class="auth-subtitle mb-5">Login dengan data yang diberikan wali kelas.</p>
 
-            @if (session('error'))
-            <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{session('error')}}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    @if (session('error'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{ session('error') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                aria-label="Close"></button>
+                        </div>
+                    @endif
+
+                    <form action="{{ route('login.proces') }}" method="POST">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" placeholder="NIS" name="nis">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" placeholder="Password"
+                                name="password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-3">Login</button>
+                    </form>
+                </div>
             </div>
-            @endif
-
-            <form action="{{ route('login.proces') }}" method="POST">
-                @csrf
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" class="form-control form-control-xl" placeholder="NIS" name="nis">
-                    <div class="form-control-icon">
-                        <i class="bi bi-person"></i>
+            <div class="col-lg-7 d-none d-lg-block d-flex">
+                <div id="auth-right">
+                    <div class="gambar">
+                        <img src="{{ asset('images/login.png') }}" alt="">
                     </div>
                 </div>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-xl" placeholder="Password" name="password">
-                    <div class="form-control-icon">
-                        <i class="bi bi-shield-lock"></i>
-                    </div>
-                </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-3">Login</button>
-            </form>
+            </div>
         </div>
-    </div>
-    <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
-            <img src="{{ asset('images/voting.gif') }}" alt="" width="900" >
-        </div>
-    </div>
-</div>
 
     </div>
 
