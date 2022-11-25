e73249a236e46ab6f655464c42e0e6a112931f83
erikfebrian62@smk.belajar.id <108683885+erikfebrian62@users.noreply.github.com>
2022-11-25 15:02:03 +0700

    Perbaikan

--- resources/views/layouts/mainadmin.blade.php
+++ resources/views/layouts/mainadmin.blade.php
@@ -39,11 +39,10 @@
             <footer>
                 <div class="footer clearfix mb-0 text-muted">
                     <div class="float-start">
-                        <p>2022 &copy; SMK Pertiwi</p>
-                    </div>
-                    <div class="float-end">
-                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                                href="https://saugi.me">Budak Angon</a></p>
+                        @php
+                        $year = date('Y');
+                        @endphp
+                        <p>{{ $year }} &copy; SMK Pertiwi</p>
                     </div>
                 </div>
             </footer>
